21155434c1c49cda9806e46d5c278cddef095b2e
Matan Yemini <matan@engageli.com>
2024-02-01 18:05:30 +0000

    Update main test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 
 
+from loaders.pptx_loader import PPTXLoader
 from slides_builder.slides_builder import SlidesBuilder
+import json
 
 
 ENGAGELI_PREFIX = "engageli-op"
@@ -9,17 +11,27 @@ SINGLE_CHOICE_POLL_QR_DATA = f"{ENGAGELI_PREFIX}://{{'op': 'PBM'}}"
 
 def main():
     print("Hello World!")
-    builder = SlidesBuilder("test2.pptx")
-    layouts = builder.inspect_slide_layouts()
+    builder = SlidesBuilder("../tests/inputs/test2.pptx")
+    
     qr_data = SINGLE_CHOICE_POLL_QR_DATA
     text_dict = {
         0: "Drinking diet coke",
-        1: "Eating Häagen-Dazs ice cream",
+        1: "Eating Haagen-Dazs ice cream",
         2: "Smoking cigarettes and drinking alcohol",
         3: "Don't organize the kitchen as you were told to do"
     }
-    builder.add_qr_slide(qr_data, text_dict, "What is more dangerous?")
-    builder.save("test2-res.pptx")
+    
+    builder.add_qr_slide(qr_data=qr_data, text_dict=text_dict, slide_title="What is more dangerous?")
+    slide = builder.add_full_slide_image(image_path="../tests/inputs/logo.png", slide_index=2)
+    builder.append_notes_to_slide(slide=slide, notes=json.dumps(text_dict))
+    
+    notes = PPTXLoader.extract_text_from_notes_between_delimiters(slide=slide)
+    print(json.loads(notes[0]))
+    
+    builder.save("../tests/outputs/test2.pptx")
+    
+
+
     
 if __name__ == "__main__":
     main()
